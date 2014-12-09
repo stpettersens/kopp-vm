3ff767e5210eaa1292f01715a50102997b2a2778
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2014-12-09 00:41:42 +0000

    Good progress. Need to fix cp_size calc though.

--- Simple.java
+++ Simple.java
@@ -2,7 +2,12 @@
 	Another very simple Java program.
 */
 class Simple {
+
+	private static int another() {
+		return 4;
+	}
 	public static void main(String[] args) {
 		int a = 3 * 2;
+		int b = another();
 	}
 }
